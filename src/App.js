33918cf1539749d123283508a7f8891a4e91ff2b
Berkeczi Arnold <arnoldberkeczi@gmail.com>
2020-03-18 21:37:36 +0200

    added about- and contactpage with route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
 import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
 class App extends React.Component {
 
@@ -15,7 +18,7 @@ class App extends React.Component {
       title: 'Berkeczi Arnold',
       headerLinks: [
         { title: 'Home', path: '/' },
-        { title: 'About', path: '/about ' },
+        { title: 'About', path: '/abbbout ' },
         { title: 'Contact', path: '/contact' }
       ],
       home: {
@@ -50,6 +53,10 @@ class App extends React.Component {
               </Nav>
             </Navbar.Collapse>
             </Navbar>
+
+            <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+            <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+            <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
 
           <Footer />
 
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AboutPage(props) {
+
+    return(
+        <p>AboutPAge works </p>
+    );
+
+}
+
+export default AboutPage;
