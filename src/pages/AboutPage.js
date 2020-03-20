14dd0e320b69e86a8ae0addfa3f1e0529ba195e7
Berkeczi Arnold <arnoldberkeczi@gmail.com>
2020-03-20 19:01:00 +0200

    Building the about page.

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Content from '../components/Content';
 
 function AboutPage(props) {
 
     return(
-        <p>AboutPAge works </p>
+        <div>
+            <Hero title={props.title}/>
+
+        <Content>
+            <p>Hello Berkeczi Arnold vagyok</p>
+            <p>Marosvásárhelyen a Sapientia egyetemen járok</p>
+        </Content>
+
+        </div>
+        
     );
 
 }
